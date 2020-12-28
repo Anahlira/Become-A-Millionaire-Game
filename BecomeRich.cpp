d523cd5c7293ca946f9ff5cba3676dd71f15d489
Anahlira <Anahlira@users.noreply.github.com>
2020-12-28 15:20:57 +0200

    Successfully retrieve the question and answers

--- BecomeRich.cpp
+++ BecomeRich.cpp
@@ -3,9 +3,76 @@
 #include <string>
 #include <stdlib.h> // rand
 #include <unistd.h> // sleep
-#include <ctime>
+#include <ctime>    //srand
 
 using namespace std;
+
+int DifrentHardnesses (ifstream& category, int currentHardnesL);
+string getAnswers (string line, int& i);
+
+struct Question {
+    string question;
+	string trueAns;
+	string ans1;
+	string ans2;
+	string ans3;
+};
+
+int main() {
+    Question q;
+    int hardnesLevel = 49;
+    int numberHardnesses = 2;
+
+    srand((unsigned int)time(NULL));// for more precise random number
+
+    ifstream categoryFile;
+    categoryFile.open("Painting.txt", ios::in);
+    if (categoryFile.is_open()) {
+        numberHardnesses = DifrentHardnesses(categoryFile, hardnesLevel);
+
+        string line;
+        int randomQuest = rand() % numberHardnesses; //choses one question
+        int currentQuest = 0; // track current question
+
+        cout<<"randomQuest: "<< randomQuest<<endl;
+
+        while (getline(categoryFile, line)) {
+            int quetionHardness = line[0];
+
+            if (quetionHardness == hardnesLevel) {
+                if(currentQuest == randomQuest) {
+                    int i = 2;
+                    string sentance;
+                    while(line[i] != '?') {
+                       sentance = sentance + line[i];
+                       i++;
+                    }
+                    q.question = sentance + '?';
+                    cout<<q.question<<endl;
+
+                    q.trueAns = getAnswers(line, ++i);
+                    q.ans1 = getAnswers(line, ++i);
+                    q.ans2 = getAnswers(line, ++i);
+                    q.ans3 = getAnswers(line, ++i);
+
+                    sentance.clear();
+                    sentance = 'a' + q.ans1;
+                    cout<<sentance<<endl;
+
+                    currentQuest++;
+                }
+                else currentQuest++;
+            }
+        }
+    }
+    else {
+        cout << "An error has ocure during the opening of the file!" << endl;
+    }
+
+    categoryFile.close();
+
+    return 0;
+}
 
 //calculates how many of 1 hardness level is there
 int DifrentHardnesses (ifstream& category, int currentHardnesL) {
@@ -18,55 +85,19 @@ int DifrentHardnesses (ifstream& category, int currentHardnesL) {
             numHardnesses++;
     }
 
+    //return to first line
     category.clear();
     category.seekg(0);
 
     return numHardnesses;
 }
 
-struct Question {
-    string question;
-	string trueAns;
-	string ans1;
-	string ans2;
-	string ans3;
-};
-
-int main() {
-    int hardnesLevel = 49;
-    int numberHardnesses = 2;
-
-    srand((unsigned int)time(NULL));// for more precise random number
-
-    ifstream category;
-    category.open("Painting.txt", ios::in);
-    if (category.is_open()) {
-        numberHardnesses = DifrentHardnesses(category, hardnesLevel);
-
-        string line;
-        int randomQuest = rand() % numberHardnesses; //choses one question
-        int i = 0; // track current question
-
-        cout<<"randomQuest: "<< randomQuest<<endl;
-
-        while (getline(category, line)) {
-            int quetionHardness = line[0];
-
-            if (quetionHardness == hardnesLevel) {
-                if(i == randomQuest) {
-                    cout<<line<<endl;
-                    i++;
-                }
-                else i++;
-            }
-            else cout<< "Sorry" <<endl;
-        }
+string getAnswers (string line, int& i) {
+    string sentance = ")";
+    while(line[i] != '/') {
+        sentance = sentance + line[i];
+        i++;
     }
-    else {
-        cout << "An error has ocure during the opening of the file!" << endl;
-    }
-
-    category.close();
-
-    return 0;
+    cout<<sentance<<endl;
+    return sentance;
 }
